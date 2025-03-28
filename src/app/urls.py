65aeb351cfbf00104914efb3accb18e073872ad8
Allama2112 <alexander.scalcione@gmail.com>
2025-03-28 12:20:39 -0400

    Merged main into this branch in order to add supabase connection

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),  # Maps 'app/' to views.index
+    path("", views.index, name="index"),
+    path("upload/", views.upload_image, name="upload_image"),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'app/index.html')
+
+def upload_image(request):
+    if request.method == "POST" and request.FILES.get("image"):
+        if not request.FILES.get("image"):
+            return render(request, "app/upload_image.html", {"error": "No file selected. Please upload an image."})
+    return render(request, "app/upload_image.html")
