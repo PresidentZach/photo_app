9993abbd0b195726706b47799ff20df527c932b7
Allama2112 <alexander.scalcione@gmail.com>
2025-03-29 12:46:54 -0400

    Now works with uploading multiple files

--- src/app/views.py
+++ src/app/views.py
@@ -7,26 +7,38 @@ def index(request):
 
 def upload_image(request):
     if request.method == "POST":
+
         # If there is no file selected
         if not request.FILES.get("image"):
             return render(request, "app/upload_image.html", {"error": "No file selected. Please upload an image."})
         
-        # Getting the actual image
-        image = request.FILES.get("image")
-
-        # If the file type is incorrect
-        if image.content_type not in ["image/jpeg", "image/png", "image/jpg"]:
-            return render(request, "app/upload_image.html", {"error": "Incorrect file type. "
-            "Please enter a .jpeg, .jpg, or .png image"})
-        tags, scores = get_tags(image)
-        print(f"tags: ", tags)
-        print(f"scores: ", scores)
-
-        tags_and_scores = zip(tags, scores)
-
+        # Getting all the images
+        images = request.FILES.getlist('image')
+        
+        # Initializing context
         context = {
-            'tags': tags_and_scores
+            "images_data": []
         }
+
+        for image in images:
+
+            # If the file type is incorrect
+            if image.content_type not in ["image/jpeg", "image/png", "image/jpg"]:
+                return render(request, "app/upload_image.html", {"error": "Incorrect file type. "
+                "Please enter a .jpeg, .jpg, or .png image"})
+            
+            tags, scores = get_tags(image)
+            print(f"tags: ", tags)
+            print(f"scores: ", scores)
+
+            tags_and_scores = zip(tags, scores)
+
+            image_data = {
+                "name": image.name,
+                "tags_and_scores": zip(tags, scores)
+            }
+
+            context["images_data"].append(image_data)
 
         
     # If no errors, render upload_image.html
