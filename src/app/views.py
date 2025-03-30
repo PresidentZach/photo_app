30828ef1d61cad33065685bdaf7c996a845a28c0
Allama2112 <alexander.scalcione@gmail.com>
2025-03-29 22:45:39 -0400

    Fixing imports

--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from classes.photo import get_tags
+from app.classes.photo import get_tags
 
 def index(request):
     return render(request, 'app/index.html')
