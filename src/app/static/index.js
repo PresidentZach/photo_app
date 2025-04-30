6f789933cff4faf03c265f0a5333c78720eeb480
fio rigney <142911636+fr572@users.noreply.github.com>
2025-04-30 12:40:03 -0400

    draft fn for displaying photos, test section in index.html for testing js

--- src/app/static/index.js
+++ src/app/static/index.js
@@ -1,3 +1,15 @@
-function displayPhotos() {
-    
+function displayPhoto() {
+
+    var photourls = "{{url_list}}";
+    var text = ""
+
+    for (let p = 0; p < photourls.length; p++ ) {
+        text += photourls[i] + "<br>";
+    }
+
 }
+
+
+function testFn() {
+    document.getElementById("test").innerHTML = "test worked!";
+  }
