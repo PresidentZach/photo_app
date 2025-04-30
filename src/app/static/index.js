6cdfc61df005fe3e63cb5e2532ed47b2f4280074
fio rigney <142911636+fr572@users.noreply.github.com>
2025-04-30 19:57:17 -0400

    fixing displayPhoto fn again...

--- src/app/static/index.js
+++ src/app/static/index.js
@@ -3,23 +3,20 @@
 function displayPhoto() {
 
     var photourls = "{{url_list}}";
-    var imagegrid = document.getElementByClassName("image-grid");
+    const imagegrid = document.getElementById("grid");
 
 
 // Loop through the photourls array, for each one create an image-box div 
 
-    for (let p = 0; p < photourls.length; p++ ) {
-        imgbox = document.createElement("div");
-        imgbox.setAttribute("class", "image-box");
+    for (let p = 0; p < photourls; p++ ) {
+        
+        //imgbox = document.createElement("div");
+        //imgbox.setAttribute("class", "image-box");
         // create an img thumbnail inside with the src attribute of the current list item
-        div.innerHTML = '<img class="thumb" src="'+ photourls[i]+ '"/></img>';
+        //div.innerHTML = '<img class="thumb" src="'+ photourls[i]+ '"/></img>';
         // add this div to image-grid
-        imagegrid.appendChild(imagegrid);
+        //imagegrid.appendChild(imgbox);
     }
 
 }
 
-// test function (remove later)
-function testFn() {
-    document.getElementById("test").innerHTML = "test worked!";
-  }
