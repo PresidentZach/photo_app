097c753a371f9e6c0f8f64a4a6a3301c57693055
Zach Stofko <90708909+PresidentZach@users.noreply.github.com>
2025-05-01 11:06:23 -0400

    changed from zach to add a event listener
    I don't know if this will fix it just passing it onward.

--- src/app/static/index.js
+++ src/app/static/index.js
@@ -1,5 +1,6 @@
 // Function to display urls fetched by url_list in views.py, and create a image_box element for each one
 
+/*
 function displayPhotos() {
 
     // Somehow, we get the url_list array from views.py...
@@ -17,5 +18,27 @@ function displayPhotos() {
     imagegrid.appendChild(imgbox);
    }
 
-}
+}*/
 
+document.addEventListener("DOMContentLoaded", function() {
+
+   function displayPhotos() {
+
+      // Somehow, we get the url_list array from views.py...
+     const imagegrid = document.getElementById("grid");
+     //var photourls = ??
+     
+  
+   // Loop through the photourls array, for each one create an image-box div 
+   // For now, it's just creating 5 divs
+     for (let p = 0; p < 5; p++ ) {
+      
+      imgbox = document.createElement("div");
+      imgbox.setAttribute("class", "image-box");
+      //imgbox.innerHTML = '<img src= "'+ photourls.[i]+'"></img>';
+      imagegrid.appendChild(imgbox);
+     }
+  
+  }
+
+});
